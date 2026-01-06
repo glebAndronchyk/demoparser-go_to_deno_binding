d8ac367fa553f19b575c49f5c215da459ddd7dff
Gleb Andronchyk <gleb.andronchyk@gmail.com>
2026-01-07 01:33:19 +0200

    added basic gamestate parsing

--- src/go/main.go
+++ src/go/main.go
@@ -1,47 +1,39 @@
 package main
 
 import (
-	"bytes"
-	// "encoding/json"
+	"demo-parsing-binding/packages/dtos"
+	parser_singleton "demo-parsing-binding/packages/singleton"
 	"fmt"
 	"syscall/js"
-
-	Parser "github.com/markus-wa/demoinfocs-golang/v5/pkg/demoinfocs"
 )
 
 func main() {
 	c := make(chan struct{}, 0)
 
-	REGISTER_API();
-	fmt.Println("WASM Go Initialized");
+	js.Global().Set("CreateParserInstance", js.FuncOf(CreateParserInstance))
+	js.Global().Set("GetGameState", js.FuncOf(GetGameState))
+	fmt.Println("WASM Go Initialized")
 
-	<-c;
+	<-c
 }
 
-func REGISTER_API() {
-	js.Global().Set("get_game_state", js.FuncOf(EXPOSE_get_game_state))
+// #region public
+
+func CreateParserInstance(_ js.Value, args []js.Value) interface{} {
+	parser_singleton.Init(args[0])
+	return nil
 }
 
-func uint8ArrayToBytes(value js.Value) []byte {
-	s := make([]byte, value.Get("byteLength").Int())
-	js.CopyBytesToGo(s, value)
-	return s
+func GetGameState(this js.Value, args []js.Value) interface{} {
+	getGameState(args[0])
+	return nil
 }
 
-func EXPOSE_get_game_state(this js.Value, args []js.Value) interface {} {
-	PRIVATE_get_game_state(args[0], args[1]);
-	return nil;
-}
+// #region private
 
-func PRIVATE_get_game_state(data js.Value, callback js.Value) {
-	b := bytes.NewBuffer(uint8ArrayToBytes(data));
-	parser := Parser.NewParser(b);
-	err := parser.ParseToEnd();
+func getGameState(callback js.Value) {
+	gameState := parser_singleton.GetInstance().GameState()
+	dto := dtos.NewGameStateDto(gameState)
 
-	if err != nil {
-		fmt.Println("Error marshaling JSON:", err);
-		return;
-	}
-
-	callback.Invoke(parser.GameState().IngameTick());
+	callback.Invoke(js.ValueOf(dto))
 }
